1f1e68413403431a04edbc3419cb8c4666f180dd
dulajkavinda <dulajkaavinda@gmail.com>
2019-08-14 13:01:35 +0530

    Added landing screen

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Platform, Image, ImageBackground } from "react-native";
+import { Button } from "native-base";
+
+let myBackground = require("./assets/icons/landing.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <ImageBackground source={myBackground} style={styles.imageBackground}>
+          <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
+            <Button block={true} style={styles.buttonStyle} onPress={() => {}}>
+              <Text style={styles.buttonText}>Start Searching</Text>
+            </Button>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? 24 : 0
   },
-});
+  viewStyle: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  imageBackground: {
+    width: "100%",
+    height: "100%"
+  },
+  titleStyle: {
+    fontSize: 30,
+    color: "blue",
+    alignItems: "center"
+  },
+  buttonStyle: {
+    margin: 10
+  },
+  buttonText: {
+    color: "white"
+  }
+};
